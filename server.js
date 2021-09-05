7efbbbd8c7a84aa86d06e4eb38a5bb9a3d56fcb1
awee01 <85651950+awee01@users.noreply.github.com>
2021-09-05 18:11:22 -0400

    Final edit + commit of server including delete

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const fs = require("fs");
+const notes = require("./db/db.json");
+const path = require("path");
+
+
+const app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static("./public"))
+
+// API calls
+app.get("/api/notes", (req, res) => {
+res.sendFile(path.join(__dirname, "./db/db.json"))
+});
+
+app.post("/api/notes", (req, res) => {
+
+      req.body.id = notes.length.toString();
+      const note = req.body;
+      notes.push(note)
+
+      fs.writeFileSync(
+      path.join(__dirname, "./db/db.json"),
+      JSON.stringify(notes, null, 2));
+      res.json(notes)
+
+});
+
+
+//Delete Request
+app.delete("/api/notes/:noteid", (req, res) => {
+      const noteid = req.params.noteid
+      for (let index = 0; index < notes.length; index++) {
+      if (notes[index].id === noteid) {
+      notes.splice(index, 1)
+      }}
+
+      fs.writeFileSync(
+      path.join(__dirname, "./db/db.json"),
+      JSON.stringify(notes, null, 2));
+      res.json(notes)
+
+});
+
+// HTML calls
+app.get("/", (req, res) => {
+res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.listen(PORT, () => {
+console.log(`API server now on port ${PORT}!`);
+});

8a87d955fdfa41b26db2896ee3da593107940af9
awee01 <85651950+awee01@users.noreply.github.com>
2021-09-19 22:25:43 -0400

    removed true sync that caused error

--- server.js
+++ server.js
@@ -1,59 +1,17 @@
-const express = require("express");
-const fs = require("fs");
-const notes = require("./db/db.json");
-const path = require("path");
+const express = require('express');
+const routes = require('./routes');
+// import sequelize connection
+const sequelize = require('./config/connection');
+
 const app = express();
-var PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(express.static("./public"))
+app.use(express.urlencoded({ extended: true }));
 
-// API calls
-app.get("/api/notes", (req, res) => {
-res.sendFile(path.join(__dirname, "./db/db.json"))
-    
-});
+app.use(routes);
 
-app.post("/api/notes", (req, res) => {
-
-    req.body.id = notes.length.toString();
-    const notetext = req.body;
-    notes.push(notetext)
-
-    fs.writeFileSync("./db/db.json", JSON.stringify(notes))
-    res.json(notes)
-
-});
-
-
-//Delete Request
-app.delete("/api/notes/:noteid", (req, res) => {
-
-    const noteid = req.params.noteid
-    for (let index = 0; index < notes.length; index++) {
-    if (notes[index].id === noteid) {
-        notes.splice(index, 1)
-    }}
-
-    fs.writeFileSync("./db/db.json", JSON.stringify(notes))
-    res.json(notes)
-
-});
-
-
-// HTML calls
-app.get("/", (req, res) => {
-res.sendFile(path.join(__dirname, "./public/index.html"));
-    
-});
-
-app.get("/notes", (req, res) => {
-res.sendFile(path.join(__dirname, "./public/notes.html"));
-    
-});
-
+// sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
-console.log(`API server now on port ${PORT}!`);
-    
+  console.log(`App listening on port ${PORT}!`);
 });
